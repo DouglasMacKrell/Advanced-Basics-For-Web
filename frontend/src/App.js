c11370c8cb1762d2acf58a411d2d711bc9876a04
DouglasMacKrell <douglasmackrell@pursuit.org>
2022-02-15 22:19:34 -0500

    Farmed first and second page and font settings and stylings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef, useEffect } from "react";
 
 function App() {
+    let videoContainer = [
+      "./closeup-coding.mp4",
+      "./closeup-coding2.mp4",
+      "./closeup-coding3.mp4",
+      "./closeup-coding4.mp4",
+      "./closeup-coding5.mp4",
+      "./closeup-coding6.mp4",
+      "./closeup-coding7.mp4",
+      "./closeup-coding8.mp4",
+    ];
+
+    let random = Math.floor(Math.random() * 8);
+
+    let randomVideo = videoContainer[random];
+
+
+  gsap.registerPlugin(ScrollTrigger);
+  const ref = useRef();
+
+  useEffect(() => {
+    const element = ref.current;
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: element.querySelector(".front-page"),
+        start: "top",
+        end: "100%",
+        // markers: true,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    tl.fromTo(
+      element.querySelector(".front-page"),
+      { clipPath: "circle(5%)" },
+      { clipPath: "circle(75%)", duration: "3" }
+    );
+
+    tl.fromTo(
+      element.querySelector(".pursuit-logo"),
+      { scale: "1" },
+      { scale: "0", opacity: "0", duration: "1" },
+      "-=3"
+    );
+
+    tl.fromTo(
+      element.querySelector(".title"),
+      { opacity: "0" },
+      { opacity: "1", duration: "1" }
+    );
+
+    tl.fromTo(
+      element.querySelector(".sub-title"),
+      { opacity: "0" },
+      { opacity: "1", duration: "1" }
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={ref}>
+      <div className="p1__wrapper">
+        <div className="front-page">
+          <div className="intro-text">
+            <h1 className="title">Advanced Basics For Web</h1>
+            <h2 className="sub-title">
+              Jordan Manley's ongoing classwork and curriculum for Pursuit's job
+              readiness program
+            </h2>
+          </div>
+          <div className="blend"></div>
+          <img
+            className="pursuit-logo"
+            src="./pursuit-monogram-black.svg"
+            alt="Pursuit logo"
+          />
+          <video src={randomVideo} autoPlay loop muted />
+        </div>
+      </div>
+      <div className="second-page">
+        <h2 className="second-page__title">Archive</h2>
+      </div>
     </div>
   );
 }
